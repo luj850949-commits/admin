ed20457877d7d6a96c30a2e54cf920d8e3a6ea52
第四天灾 <luj850949@gmail.com>
2026-03-17 19:29:50 +0800

    完成配置路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,4 +25,29 @@ const router = createRouter({
   ],
 })
 
+// 路由白名单
+const whiteList = ['/login'];
+// 全局路由守卫
+router.beforeEach((to, from, next) => {
+  // 从本地存储中尝试获取 Token
+  const token = localStorage.getItem('token');
+  if (token) {
+    if (to.path === '/login') {
+      // 如果他已经登录了，还访问login，则直接踢回首页
+      next('/'); 
+    } else {
+      // 去其他业务页面，直接放行！
+      next(); 
+    }
+  } else {
+    if (whiteList.includes(to.path)) {
+      // 如果他去的是登录页等白名单里的页面，直接放行
+      next(); 
+    } else {
+      // 其他页面直接踢回登录页
+      next('/login'); 
+    }
+  }
+});
+
 export default router
